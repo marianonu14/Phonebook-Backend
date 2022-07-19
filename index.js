497c9a62da275cc819ffeb1d05af5f21a4f14fe8
Mariano <marianonunezma@gmail.com>
2022-07-19 11:30:41 -0300

    Add catch error on post

--- index.js
+++ index.js
@@ -28,23 +28,19 @@ app.get('/api/persons/:id', (request, response, next) => {
 		.catch(error => next(error));
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
 	const body = request.body;
   
-	if (!body.name) {
-		return response.status(400).json({ 
-			error: 'name missing' 
-		});
-	}
-
 	const person = new Person({
 		name: body.name,
 		number: body.number
 	});
 
-	person.save().then(savedPerson => {
-		response.json(savedPerson);
-	});
+	person.save()
+		.then(savedPerson => {
+			response.json(savedPerson);
+		})
+		.catch(error => next(error));
 });
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -70,7 +66,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 		.catch(error => next(error));
 });
 
-app.use((request, response, next) => {
+app.use((request, response) => {
 	response.status(404).end();
 });
 
@@ -79,7 +75,9 @@ app.use((error, request, response, next) => {
   
 	if (error.name === 'CastError') {
 		return response.status(400).send({ error: 'malformatted id' });
-	} 
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message });
+	}
   
 	next(error);
 });
